b5a9ccc4b937e98031716ca6e0e690618da312a6
Yanitoledo <yaninatoledo8@gmail.com>
2019-08-29 23:15:27 -0300

    Added notification and header

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min';
-import 'popper.js/dist/popper'
-import 'jquery/dist/jquery.min';
+import 'popper.js/dist/popper';
+import 'jquery/dist/jquery';
 import 'bootstrap/dist/js/bootstrap.min';
